71ca1ace8384fd521f979947185d94611c2bfc27
Tea Flores-Kestner <dfkestner@gmail.com>
2020-10-07 14:45:22 -0400

    updates search features

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,43 +17,43 @@ $(document).ready(function() {
 
                 for (i = 0; i < response.cuisines.length; i++) {
                     var cuisBtn = $("<button>").text(response.cuisines[i].cuisine.cuisine_name).addClass("cuisineBtn").attr("data-index", response.cuisines[i].cuisine.cuisine_id); 
-                    $(".searchOptions").append(cuisBtn)
+                    $(".searchOptions").append(cuisBtn);
                 }
 
                 $(".cuisineBtn").on("click", function() {
-                    var cuisID = $(this).data("index")
+                    var cuisID = $(this).data("index");
                     console.log(cuisID);
                     console.log($(this).data("index"));
 
                     cuisineList(cuisID, lat, long)
                 })
+            }
+        })
+    }
 
-                function cuisineList(cuisineID, lat, long) {
-                    var apiKey = "b719894d13610808dbf09abce78bb1ea";
-                    var queryURL = "https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + long + "&cuisines=" + cuisineID
+    function cuisineList(cuisineID, lat, long) {
+        var apiKey = "b719894d13610808dbf09abce78bb1ea";
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + long + "&cuisines=" + cuisineID
 
-                    $.ajax({
-                        dataType: "json",
-                        url: queryURL,
-                        method: "GET",
-                        headers: {
-                        'user-key': apiKey,
-                        },
-                        success: function (response) {
-                            console.log(response);
-                            console.log(lat, long, cuisineID);
+        $.ajax({
+            dataType: "json",
+            url: queryURL,
+            method: "GET",
+            headers: {
+            'user-key': apiKey,
+            },
+            success: function (response) {
+                console.log(response);
+                console.log(lat, long, cuisineID);
 
-                            for (i = 0; i < response.restaurants.length; i++) {
-                                var rName = $("<p>").text(response.restaurants[i].restaurant.name)
-                                var hours = $("<p>").text(response.restaurants[i].restaurant.timings)
-                                var phone = $("<p>").text(response.restaurants[i].restaurant.phone)
-                                var website = $("<p>").text(response.restaurants[i].restaurant.url)
-                                var location = $("<p>").text(response.restaurants[i].restaurant.location.address)
+                for (i = 0; i < response.restaurants.length; i++) {
+                    var rName = $("<p>").text(response.restaurants[i].restaurant.name);
+                    var hours = $("<p>").text(response.restaurants[i].restaurant.timings);
+                    var phone = $("<p>").text(response.restaurants[i].restaurant.phone);
+                    var website = $("<p>").text(response.restaurants[i].restaurant.url);
+                    var location = $("<p>").text(response.restaurants[i].restaurant.location.address);
 
-                                $(".restaurantList").append(rName, hours, phone, website, location)
-                            }
-                        }
-                    })
+                    $(".restaurantList").append(rName, hours, phone, website, location);
                 }
             }
         })
@@ -75,14 +75,50 @@ $(document).ready(function() {
 
                 var longitude = response.results[0].locations[0].latLng.lng
                 console.log(longitude)
+                window.localStorage.setItem("longitude", JSON.stringify(longitude));
 
                 var latitude = response.results[0].locations[0].latLng.lat
                 console.log(latitude)
+                window.localStorage.setItem("latitude", JSON.stringify(latitude));
 
                 cuisines(latitude, longitude)
+
+                searchQuery(newSearch(), latitude, longitude)
             }  
         })       
-    }            
+    }
+    
+    function newSearch() {
+        $(".searchOptions").text("Search for a dish or pick a cuisine:")
+        $("#searchButton").attr("placeholder", "")
+        $("#searchButton").on("click", function(event) {
+            event.preventDefault();
+            var query = $("#searchZip").val().trim();
+            searchQuery(query)
+            $("#searchZip").val("");
+        })
+    }
+
+    function searchQuery(query, lat, long) {
+        var apiKey = "b719894d13610808dbf09abce78bb1ea";
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + query + "&lat=" + lat + "&lon=" + long
+        console.log(query, lat, long);
+
+        $.ajax({
+            dataType: "json",
+            url: queryURL,
+            method: "GET",
+            headers: {
+            'user-key': apiKey,
+            },
+            success: function (response) {
+                console.log(response);
+                
+            
+                
+            }
+        })
+    }
 
     $("#searchButton").on("click", function(event) {
         event.preventDefault();
@@ -93,82 +129,4 @@ $(document).ready(function() {
 
         $("#searchZip").val("");
     })
-
-    // var catButton = $("<button>").text("Search by Category").addClass("catSearch").attr("id", "catSearch");
-    // $(".catList").append("<p id='category'>");
-    // $("#category").append(catButton);
-
-    // catButton.on("click", function(event){
-    //     event.preventDefault();
-
-    //     var apiKey = "b719894d13610808dbf09abce78bb1ea";
-    //     var queryURL = "https://developers.zomato.com/api/v2.1/categories"
-
-    //     $.ajax({
-    //         dataType: "json",
-    //         url: queryURL,
-    //         method: "GET",
-    //         headers: {
-    //             'user-key': apiKey,
-    //         },
-    //         success: function (response) {
-    //             console.log(response);
-
-    //             var category = response.categories;
-
-    //             for (i = 0; i < category.length; i++) {
-    //                 var catButton2 = $("<button>").text(category[i].categories.name).addClass("categoryName").val(category[i].categories.id);
-    //                 $("#catSearch").append(catButton2);
-    //                 console.log(category[i].categories.name);
-    //                 console.log(category[i].categories.id);
-
-    //                 var catID = category[i].categories.id;
-    //             }
-
-    //             $(".categoryName").on("click", function(event) {
-    //                 event.preventDefault();
-
-    // //                 // var sortBy = $("<ul>").text("Sort by")
-    // //                 // (catButton2).append(sortBy);
-    // //                 // var cost = $("<button>").text("Cost").val(cost);
-    // //                 // var rating = $("<button>").text("Rating").val(rating);
-    // //                 // var distance = $("<button>").text("Distance").val(real_distance);
-    // //                 // add &sort= after type number
-
-    //                 var apiKey = "b719894d13610808dbf09abce78bb1ea";
-    //                 var queryURL = "https://developers.zomato.com/api/v2.1/search?establishment_type" + catID;
-
-    //                 $.ajax({
-    //                     dataType: "json",
-    //                     url: queryURL,
-    //                     method: "GET",
-    //                     headers: {
-    //                         'user-key': apiKey,
-    //                     },
-    //                     success: function (response) {
-    //                         console.log(response);
-                    
-    //                     }
-    //                 })
-    //             })
-    //         }
-    //     });
-    // })
-
-     // function collections(locID) {
-    //     var apiKey = "b719894d13610808dbf09abce78bb1ea";
-    //     var queryURL = "https://developers.zomato.com/api/v2.1/collections?city_id=" + locID
-
-    //     $.ajax({
-    //         dataType: "json",
-    //         url: queryURL,
-    //         method: "GET",
-    //         headers: {
-    //         'user-key': apiKey,
-    //         },
-    //         success: function (response) {
-    //             console.log(response);
-    //         }
-    //     })
-    // }
 })
